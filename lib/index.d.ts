e51de8919aecf2890742e8687fa27d8ec25fa5f9
krarpit <kumar.arpit77@yahoo.com>
2025-03-28 13:37:38 -0700

    re-compile

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,18 +1,67 @@
-export type Constraint<T> = (vars: Record<string, T>) => boolean;
+/**
+ * Lamb(iguous)
+ * A generalized backtracking search library
+ *
+ * https://github.com/kumarpit/lamb.js
+ */
+/**
+ * Represents a constraint function that evaluates a set of variable assignments
+ * @template T The type of values that variables can take
+ * @param vars A record of variable names to their values
+ * @returns true if the given assignment satisfies the constraint, else false
+ */
+export type Constraint<T> = (vars: Assignment<T>) => boolean;
+/**
+ * Represents the possible choices for any given variable
+ * @template T The type of values that variables can take
+ */
 export interface Choices<T> {
     [variable: string]: T[];
 }
+/**
+ * Represents an assignment for variables
+ * @template T The type of values that variables can take
+ */
 export interface Assignment<T> {
     [variable: string]: T;
 }
-declare class AmbSolver<T> {
+/**
+ * A generalized backtracking-based solver for constraint satisfaction problems
+ * @template T The type of values that variables can take
+ */
+declare class Lamb<T> {
     private choices;
     private constraints;
     private solutions;
+    /**
+     * Adds a variable and its possible values to the problem
+     * @param variable The name assigned to the variable
+     * @param values The possible values this variable can take
+     */
     addChoice(variable: string, values: T[]): void;
+    /**
+     * Adds a constraint to the problem
+     * @param constraint A predicate function that recieves variable assignments and determines
+     * if they satisfy some constraint
+     */
     addConstraint(constraint: Constraint<T>): void;
+    /**
+     * Solves the constraint-satisfaction problem using backtracking
+     * @returns A list of all successful assignments
+     */
     solve(): Assignment<T>[];
+    /**
+     * Orchestrates the backtracking search
+     * @param assignment The current (possibly incomplete) assignment
+     * @param variables The list of all variables involved in this problem
+     * @param index How many variables have been assigned so far
+     */
     private backtrack;
+    /**
+     * Checks if the current assignment satisfies all constraints
+     * @param assignment The current (complete) assignment (i.e every variable has been assigned a value)
+     * @returns true if all constraints are satisfied, else false
+     */
     private checkConstraints;
 }
-export default AmbSolver;
+export default Lamb;
