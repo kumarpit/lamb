c073e5e9afe3fe7b0f16db2273cca37214fb17a3
krarpit <kumar.arpit77@yahoo.com>
2025-03-28 19:48:03 -0700

    better examples

--- example.ts
+++ example.ts
@@ -1,13 +1,55 @@
 import Lamb from "./index";
 
-let solver = new Lamb<number>();
+let solver1 = new Lamb<number>();
 
-// Simple example where we find all numbers such that x + y = 7
-solver.addChoice("x", [1, 2, 3, 4, 5]);
-solver.addChoice("y", [1, 2, 3, 4, 5]);
-solver.addConstraint((vars) => vars.x + vars.y == 7);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Simple example where we want to find all pairs of numbers from the given lists that sum to 7 
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-let results = solver.solve(1);
-results.forEach((assignment) => console.log(assignment));
+solver1.addChoice("x", [1, 2, 3, 4, 5]);
+solver1.addChoice("y", [1, 2, 3, 4, 5]);
+solver1.addConstraint((vars) => vars.x + vars.y == 7);
 
-solver.clear();
+let results1 = solver1.solve();
+console.log("Pairs that sum to 7 from lists of 1..5: ");
+results1.forEach(assignment => console.log(assignment));
+
+console.log("\n");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Solving the graph coloring problem with 4 colors 
+// Problem taken from https://www.metalevel.at/prolog/optimization
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+let solver2 = new Lamb<string>();
+
+const colors = ["red", "green", "blue", "yellow"];
+let adjacencyList = {
+    "a": ["b", "c", "d", "f"],
+    "b": ["a", "c", "d"],
+    "c": ["a", "b", "d", "e"],
+    "d": ["a", "b", "c", "e", "f"],
+    "e": ["c", "d", "f"],
+    "f": ["a", "d", "e"]
+}
+
+Object.keys(adjacencyList).forEach(key => solver2.addChoice(key, colors));
+
+solver2.addConstraint((vars) => {
+    for (const key of Object.keys(adjacencyList)) {
+        let keyColor = vars[key];
+        let adjacentColors = (adjacencyList as { [_: string]: string[] })[key].map(v => vars[v]);
+        if (adjacentColors.includes(keyColor)) return false;
+    }
+    return true;
+});
+
+let results2 = solver2.solve(1);
+console.log("A solution to the map coloring problem with 4 colors: ");
+results2.forEach(assignment => console.log(assignment));
+
+console.log("\n");
